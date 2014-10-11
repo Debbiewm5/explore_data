b1f20f3268b61f201d37b750db25989eaa8590f6
Debbiewm5 <dwmstat@gmail.com>
2014-10-10 22:17:08 -0400

    plot2 code
    project1

---
+++ plot2.R
@@ -0,0 +1,13 @@
+## Reading and plotting downloaded data-plot2.
+dataFile <- "./ExData_Plotting1/household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+                subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+                #str(subSetData)
+        datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+        myData <- as.numeric(subSetData$Global_active_power)
+
+png("plot2.png", width=480, height=480)
+plot(datetime, myData, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.copy(png, file="plot2.png")
+dev.off()
+
